385d17488aff7c57279be64ff9d4e51860dca9b5
Ilias <i.elhammouti@student.fontys.nl>
2023-06-12 14:23:34 +0200

    updated program cs for new microservices

--- MAPPY_Backend/CommunityService.Api/Program.cs
+++ MAPPY_Backend/CommunityService.Api/Program.cs
@@ -1,23 +1,54 @@
+using System.Text.Json.Serialization;
+using CommunityService.Core.Api.Services;
+using CommunityService.DataAccessLayer.Context;
+using CommunityService.DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("MemberDb");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//SERVICES
+builder.Services.AddTransient<ICommunityService, CommunityService.Core.Api.Services.CommunityService>();
+builder.Services.AddScoped<ICommunityRepository, CommunityRepository>();
+
+builder.Services.AddDbContext<CommunityDbContext>(x => x.UseSqlServer(connectionString));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+    
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("*")
+                .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+        });
+});
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var y = scope.ServiceProvider.GetRequiredService<CommunityDbContext>();
+    y.Database.Migrate();
+}
 
-// Configure the HTTP request pipeline.
+app.UseDeveloperExceptionPage();
+
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
+app.UseRouting();
+// Cors
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
